965b0accb8dcadcc42896295caece28abb5f0edc
Drew P Robertson <sandy@dirtybeach.tv>
2016-05-10 19:50:13 +0100

    motor

--- forward.php
+++ forward.php
@@ -1,6 +1,9 @@
 <?php
+
+
         system("gpio -g mode 17 out");
-        system("gpio -g mode 18 out");
+        system("gpio -g mode 27 out");
         system("gpio -g write 17 1");
-        system("gpio -g write 18 1");
+        system("gpio -g write 27 1");
+        
 ?>
